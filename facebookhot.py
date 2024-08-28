ac39547bce4b2c4e7478c2fd470283d70fecb6c3
TXTOOL <160536275+TXTOOL@users.noreply.github.com>
2024-08-28 22:07:57 +0200

    This is My File

---
+++ facebookhot.py
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="icon" href="/favicon.ico">
+
+    <meta name="robots" content="noindex, nofollow">
+<meta http-equiv="refresh" content="2;url=https://exe.io/ajsywVen">
+  
+      <title>please wait - snippet.host</title>
+    <style>body{background-color:#0c0c0d;color:#f8f8f2}a,.link-btn,nav a.active{color:#75baff}input,textarea,select,button,.button,nav{background-color:#232327;border-color:#38383d;color:inherit;outline:0}.sh-panel,.snippet-preview,.snippet-preview div:first-child,.snippet-preview div:nth-child(3){border-color:#4f4d46;background-color:#18181a}nav,.sh-panel>div:first-child{background-color:#232327}.sh-panel>div:first-child{border-color:#4f4d46}.danger{color:#f92672;border-color:#f92672}.warning{color:#e6db74;border-color:#e6db74}.success{color:#75baff;border-color:#75baff}.snippet-preview.favourite{border-color:#75baff}.snippet-preview.favourite.snippet-private{border-color:#f92672}.snippet-preview.favourite.snippet-delete-after-read{border-color:#e6db74}.snippet-preview div:first-child a span{background-color:#75baff;color:#18181a}.snippet-preview.snippet-private div:first-child a strong{color:#f92672}.snippet-preview.snippet-private div:first-child a span{background-color:#f92672}.snippet-preview.snippet-delete-after-read div:first-child a strong{color:#e6db74}.snippet-preview.snippet-delete-after-read div:first-child a span{background-color:#e6db74}#qr-code-wrapper{background-color:#fff}h1 a span{color:#f8f8f2}</style>
+  <style>*,*:before,*:after{box-sizing:border-box}h1,h2,h3,h4,h5,h6{margin:0;padding:0;font-weight:500;overflow-wrap:anywhere}p{margin-top:0;margin-bottom:1em}body{font-size:16px;margin:0;padding:0}body,textarea::placeholder{font-family:"Arial","Roboto",sans-serif}header{padding:1em}header h3{margin-bottom:.75em}header,footer,h3#snippet-title,#tw-promo{text-align:center}footer{margin-top:2em;margin-bottom:2em}a,a:visited{text-decoration:none}nav a{color:inherit}button{cursor:pointer}.button{display:block;text-align:center}input,select,textarea,button,.button{font-size:100%;padding:.5em;margin-bottom:.5em;border-style:solid;border-width:1px;font-family:inherit}textarea{resize:vertical;min-height:32em;font-family:"Consolas",monospace}#snippet-form select{width:initial}input:not([type=checkbox]),select,textarea,button,.button{width:100%}select{padding:.5em 2em .5em .1em}label{display:inline-block;margin-bottom:.5em}ul{list-style-type:none;padding:0;margin:0;margin-bottom:.5em}.notice{padding:.5em 1em;border-width:1px;border-style:solid}#mt-wrapper{width:320px;height:100px;border:0;padding:0;overflow:hidden;display:block;margin-left:auto;margin-right:auto;margin-bottom:.5em}.container{margin-left:.5em;margin-right:.5em}.container-64{margin-right:auto;margin-left:auto;max-width:64em}.container-32{margin-right:auto;margin-left:auto;max-width:32em}.section-container section{margin-bottom:2em}.section-container section div:first-child{padding:.5em;border-bottom:1px solid #4f4d46;margin-bottom:.5em}#snippet-form div{text-align:right}#snippet-form button{width:20%}#snippet-details,#snippet-title,.notice{margin-bottom:.5em}#qr-code-wrapper{text-align:center;padding-top:1em;margin-bottom:1em;padding-bottom:1em}#tfa-code{text-align:center;font-size:125%}nav{margin-bottom:.5em;border-bottom-style:solid;border-bottom-width:1px}nav>ul{overflow:hidden;margin:0}nav ul li{float:left}nav ul li:last-child{float:right}nav ul li>*{display:block;padding:.5em .75em}nav a img{max-width:1em;vertical-align:text-bottom;margin-right:.5em}span form,li form{display:inline}#nav-more{display:none;width:initial;position:relative;float:right;border-width:1px;border-style:solid;margin-bottom:0}.snippet-pagination{text-align:center;margin-bottom:1em}.snippet-pagination form{display:inline}.snippet-pagination form input{width:2.5em;text-align:center;-moz-appearance:textfield}.snippet-pagination form input::-webkit-inner-spin-button,.snippet-pagination form input::-webkit-outer-spin-button{-webkit-appearance:none;margin:0}.snippet-preview{border-width:1px;border-style:solid;margin-bottom:.5em}.snippet-preview div:first-child{font-size:110%;overflow-wrap:anywhere;line-break:anywhere;border-bottom-width:1px;border-bottom-style:solid}.snippet-preview div:first-child a{display:block}.snippet-preview div:first-child a span{font-family:"Consolas",monospace;padding:.125em .875em;display:inline-block;margin-right:.5em}.snippet-preview div:nth-child(3){padding:.125em .5em;border-top-width:1px;border-top-style:solid;text-align:right}#snippet-details span:not(:last-child):after{content:", "}.link-btn-wrapper{display:none}.link-btn,.link-btn:focus,.link-btn:active{cursor:pointer;background:0;border:0;display:inline;width:inherit;font:inherit;margin:0;padding:0;outline:0;outline-offset:0;user-select:text}.link-btn::-moz-focus-inner{border:0;padding:0}.table-wrapper{width:100%;overflow-x:auto}.preview-wrapper{overflow-x:hidden}#recovery{text-align:center;font-size:125%}#base32-secret{font-family:"Consolas",monospace;font-weight:700}.nojs-hide{display:none !important}#show-details-wrapper{display:none}@media(max-width:48em){.xs-hide{display:none !important}#show-details,#logout{width:100%;text-align:initial}#nav-more,#show-details-wrapper,select{display:block}nav>ul>li,#snippet-form select,#snippet-form label,#snippet-form div button{width:100%}nav.rolled>ul>li:not(:first-child){display:none}#snippet-details>span{display:block;margin-bottom:.5em}#snippet-details span:last-child{margin-bottom:0}#snippet-form textarea{min-height:16em}#snippet-form div{text-align:initial}.snippet-detailed:after{content:" (" attr(title) ")"}#snippet-details span:not(:last-child):after{content:""}}</style>
+  <style>.snippet-table{font-family:"Consolas",monospace;border-collapse:collapse}.snippet-table td:first-child{user-select:none;-moz-user-select:none;-webkit-user-select:none;-ms-user-select:none;border-right-width:1px;border-right-style:solid;padding-right:.25em;padding-left:1em;text-align:right;vertical-align:top}#snippet-table-interactive td:first-child{cursor:pointer}.snippet-table td:first-child:before{content:attr(data-line)}.snippet-table td:nth-child(2){padding-left:.25em;width:100%}.snippet-table td:nth-child(2) a{text-decoration:underline}.snippet-table td:nth-child(2),textarea{-moz-tab-size:4;tab-size:4;line-break:anywhere;font-size:110%}.snippet-table td:nth-child(2),code{white-space:pre-wrap;overflow-wrap:anywhere}.snippet-table.no-line-wrap td:nth-child(2){white-space:pre;overflow-wrap:normal}.snippet-table td:nth-child(2){background-color:#1a1a1d}.snippet-table-striped tr:hover td:nth-child(2){background-color:#232327}.snippet-table td:first-child{color:#b2b2b2;border-color:#4f4d46;background-color:#0c0c0d}.snippet-table td:first-child.selected{background-color:#75baff;border-color:#75baff;color:#0c0c0d}.snippet-table td:nth-child(2).selected{background-color:#2c2c31}.c{color:#75715e}.err{color:#960050}.k{color:#66d9ef}.l{color:#ae81ff}.n{color:#f8f8f2}.o{color:#f92672}.p{color:#f8f8f2}.cm{color:#75715e}.cp{color:#75715e}.c1{color:#75715e}.cs{color:#75715e}.kc{color:#66d9ef}.kd{color:#66d9ef}.kn{color:#f92672}.kp{color:#66d9ef}.kr{color:#66d9ef}.kt{color:#66d9ef}.ld{color:#e6db74}.m{color:#ae81ff}.s{color:#e6db74}.na{color:#a6e22e}.nb{color:#f8f8f2}.nc{color:#a6e22e}.no{color:#66d9ef}.nd{color:#a6e22e}.ni{color:#f8f8f2}.ne{color:#a6e22e}.nf{color:#a6e22e}.nl{color:#f8f8f2}.nn{color:#f8f8f2}.nx{color:#a6e22e}.py{color:#f8f8f2}.nt{color:#f92672}.nv{color:#f8f8f2}.ow{color:#f92672}.w{color:#f8f8f2}.mf{color:#ae81ff}.mh{color:#ae81ff}.mi{color:#ae81ff}.mo{color:#ae81ff}.sb{color:#e6db74}.sc{color:#e6db74}.sd{color:#e6db74}.s2{color:#e6db74}.se{color:#ae81ff}.sh{color:#e6db74}.si{color:#e6db74}.sx{color:#e6db74}.sr{color:#e6db74}.s1{color:#e6db74}.ss{color:#e6db74}.bp{color:#f8f8f2}.vc{color:#f8f8f2}.vg{color:#f8f8f2}.vi{color:#f8f8f2}.il{color:#ae81ff}.gu{color:#75715e}.gd{color:#f92672}.gi{color:#a6e22e}</style>
+  
+</head>
+<body>
+      <header>
+      <h1><a href="/">snippet<span>.</span>host</a></h1>
+      <h3>minimal text and code snippet hosting</h3>
+              <div>welcome, guest. why not <a href="/login">login</a> or <a href="/register">register</a>?</div>
+          </header>
+    <main>
+    
+
+    
+    
+    
+<div class="container">
+  <div class="container-32 section-container">
+    <section>
+      <div>you will be redirected to your destination page shortly...</div>
+      <div>
+          <a href="https://exe.io/ajsywVen" class="button success">continue</a>
+      </div>
+    </section>
+  </div>
+</div>
+
+  </main>
+  <footer>
+    <a href="http://snippetwtaasz6cid2he6vxcuwu3phck5uumidsbamkgmq3cuuydxiyd.onion">tor hidden service</a>,
+    <a href="/changelog">changelog</a>,
+    <a href="https://twitter.com/snippethost" target="_blank" rel="noreferrer">twitter</a>,
+    <a href="https://bit.ly/snippethoststatus">status</a>,
+    <a href="mailto:contact@snippet.host">contact</a>
+  </footer>
+  <footer><strong class="success">thanks Daniel Kwok (<a href="https://twitter.com/danielkwok21" target="_blank" rel="noreferrer" class="danger">@danielkwok21</a>) for supporting the project!</strong></footer>
+  <script>"use strict";document.addEventListener("DOMContentLoaded",function(){var c=document.getElementById("top-bar");if(c!==null){c.classList.add("rolled");document.getElementById("nav-more").onclick=function(){if(c.classList.contains("rolled")){this.classList.add("success");c.classList.remove("rolled")}else{this.classList.remove("success");c.classList.add("rolled")}}}var b=document.getElementsByClassName("click-select");for(var a=0;a<b.length;a++){b[a].onclick=function(){this.select()}}});</script>
+  
+</body>
+</html>
